701eb8aa85099a1935d2be94f3d040756d66e795
Predakor <patrykbusko@gmail.com>
2023-03-03 15:50:39 +0100

    Added basic contact section

--- src/layout/Nav.tsx
+++ src/layout/Nav.tsx
@@ -8,7 +8,7 @@ function Nav({}) {
       transition={{ duration: 0.3 }}
       class={'fixed z-50 w-full p-4 text-2xl'}
     >
-      <ul class={'flex justify-end gap-4'}>
+      <ul class={'flex justify-end gap-4 '}>
         <li>
           <a href="#home">Home</a>
         </li>
--- src/sections/Contact.tsx
+++ src/sections/Contact.tsx
@@ -1,7 +1,63 @@
 function Contact() {
   return (
-    <section id={'contact'}>
-      <h2>Contact me</h2>
+    <section
+      className={'flex flex-col gap-4'}
+      id={'contact'}
+      aria-label="Contact section"
+    >
+      <h2 className="text-4xl">Get in touch</h2>
+      <div className="grid flex-1 grid-cols-2 gap-4 p-4 text-2xl">
+        <article className="flex flex-col gap-4">
+          <h3 className="text-3xl">Send me an email</h3>
+
+          <form
+            className={'flex flex-col gap-8'}
+            action="mailto:patrykbusko@gmail.com"
+            method="post"
+            encType="text"
+          >
+            <input
+              className="rounded p-2"
+              type="text"
+              id="name"
+              placeholder={'Name'}
+              required
+            />
+            <input
+              className="rounded p-2"
+              type="text"
+              id="surname"
+              placeholder={'Surname'}
+              required
+            />
+            <input
+              className="rounded p-2"
+              type="email"
+              id="email"
+              placeholder={'Email'}
+              required
+            />
+            <textarea
+              className="rounded p-2"
+              id="message"
+              cols={30}
+              rows={10}
+              placeholder="Your message"
+              required
+            />
+            <button className="bg-orange-600 p-4">Send</button>
+          </form>
+        </article>
+
+        <article className={'flex flex-col gap-4'}>
+          <h3 className="text-3xl">Socials</h3>
+          <address className="flex flex-col gap-4">
+            <a href="https://github.com/Predakor">Github</a>
+            <a href="http://">Linkedin</a>
+            <a href="http://">Facebook</a>
+          </address>
+        </article>
+      </div>
     </section>
   );
 }
